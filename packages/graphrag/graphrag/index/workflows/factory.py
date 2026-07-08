e317fc2a7b8670631ccce6aa3f9b787f698da18b
killstealing <13840935238@163.com>
2026-07-08 17:27:45 +0800

    19 GraphRAG index源码篇之finalize_graph结果查看

--- packages/graphrag/graphrag/index/workflows/factory.py
+++ packages/graphrag/graphrag/index/workflows/factory.py
@@ -54,7 +54,7 @@ _standard_workflows = [
     "create_base_text_units",
     "create_final_documents",
     "extract_graph",
-    # "finalize_graph",
+    "finalize_graph",
     # "extract_covariates",
     # "create_communities",
     # "create_final_text_units",
